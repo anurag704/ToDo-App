a76fabc88b5cd99f9945898e1ab819429924dbe9
anurag704 <anuragjha1093@gmail.com>
2020-09-21 15:38:58 +0530

    Updates

---
+++ precache-manifest.d5fe41ebe1cae12b7cd5da965c3dac37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40af31b394f11625e6fc05c9af10a465",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "ecb62ef934186a3dbff4",
+    "url": "/ToDo-App/static/css/main.e8e91e7f.chunk.css"
+  },
+  {
+    "revision": "edd9407736101e78490a",
+    "url": "/ToDo-App/static/js/2.aca5ddb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-App/static/js/2.aca5ddb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb62ef934186a3dbff4",
+    "url": "/ToDo-App/static/js/main.09e46bd9.chunk.js"
+  },
+  {
+    "revision": "933c1e139230068a8393",
+    "url": "/ToDo-App/static/js/runtime-main.34c1ab66.js"
+  }
+]);
